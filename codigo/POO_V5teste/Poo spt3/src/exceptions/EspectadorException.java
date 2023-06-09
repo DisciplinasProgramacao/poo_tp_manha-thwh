da9a485533012cec266eaa0381336782d98d223b
HenriqueCarrara0 <103524747+HenriqueCarrara0@users.noreply.github.com>
2023-06-08 22:38:19 -0300

    Delete codigo/POO_V5teste/Poo spt3 directory

--- codigo/POO_V5teste/Poo spt3/src/exceptions/EspectadorException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-import java.security.InvalidParameterException;
-
-public class EspectadorException extends InvalidParameterException {
-    public EspectadorException(String nome, String caracter) {
-        super("O "+ nome +" do Cliente deve possuir mais de "+ caracter +" caracteres!");
-}
-}
