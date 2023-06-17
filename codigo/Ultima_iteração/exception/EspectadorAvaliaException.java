1e14c7c4fa4b5aa6ec9f38ddc6e846b829e7735e
HenriqueCarrara0 <103524747+HenriqueCarrara0@users.noreply.github.com>
2023-06-17 12:32:23 -0300

    Delete codigo/Ultima_iteração directory

--- codigo/Ultima_iteração/exception/EspectadorAvaliaException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class EspectadorAvaliaException extends Exception {
-   
-
-	public EspectadorAvaliaException() {
-        super("Você precisa ter visto a mídia para poder avalia-lá");
-    }
-}
